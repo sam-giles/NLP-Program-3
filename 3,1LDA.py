2b81304c5d73d73d5b2d4c17479c28a45b8deff7
samgiles <samdotgiles@gmail.com>
2020-04-30 09:03:08 -0500

    reworked to new list format, preprocessing done while reading in

--- 3,1LDA.py
+++ 3,1LDA.py
@@ -4,38 +4,44 @@ from nltk.stem.wordnet import WordNetLemmatizer
 from gensim.corpora import Dictionary
 from nltk.corpus import stopwords
 import csv
+import string
 
 def readInDocuments(directory):
     texts = []
     for filename in os.listdir(directory):
         open_file = open(directory + filename, 'r')
         contents = open_file.readlines()
-        tempList = []
+        tempList = [filename]
+        contentList = []
         for i in range(len(contents)):
-            tempList.extend(contents[i].split())
-        texts.append(tempList)    
+            contentList.extend(contents[i].split())
         open_file.close()
+        contentList = preprocessData(contentList)
+        tempList.append(contentList)
+        texts.append(tempList)
     return texts
 
 #adapted from gensim documentation
-def preprocessData(docs):
-    for idx in range(len(docs)):
-        docs[idx] = [word.lower() for word in docs[idx]]  # Convert to lowercase.
+def preprocessData(doc):
+    doc = [word.lower() for word in doc]  # Convert to lowercase.
 
     # Remove numbers, but not words that contain numbers.
-    docs = [[token for token in doc if not token.isnumeric()] for doc in docs]
+    doc = [token for token in doc if not token.isnumeric()]
 
     # Remove words that are only one character.
-    docs = [[token for token in doc if len(token) > 1] for doc in docs]
+    doc = [token for token in doc if len(token) > 1]
 
-    lemmatizer = WordNetLemmatizer()
-    docs = [[lemmatizer.lemmatize(token) for token in doc] for doc in docs]
+    #remove punctuation
+    doc = [s.translate(str.maketrans('', '', string.punctuation)) for s in doc]
 
     #remove stopwords
     sw = set(stopwords.words('english'))
-    docs = [[word for word in text if word not in sw] for text in docs]
+    doc = [word for word in doc if word not in sw]
 
-    return docs
+    lemmatizer = WordNetLemmatizer()
+    doc = [lemmatizer.lemmatize(token) for token in doc]
+
+    return doc
 
 def readMeta():
     meshDict = {}
@@ -46,39 +52,64 @@ def readMeta():
             meshDict[row[0].strip()] = [topic.strip() for topic in row[1:]]
     return meshDict
 
-def generateTopicDocVector(metadata):
-    topic_doc_vector = {}
-    for name,topics in metadata.items():
-        for topic in topics:
-            if topic in topic_doc_vector.keys():
-                if name not in topic_doc_vector[topic]:
-                    topic_doc_vector[topic].append(name)
+def generateLabelDocVector(metadata):
+    label_topic_vector = {}
+    for name,labels in metadata.items():
+        for label in labels:
+            if label in label_topic_vector.keys():
+                if name not in label_topic_vector[label]:
+                    label_topic_vector[label].append(name)
             else:
-                topic_doc_vector[topic] = [name]
-    return topic_doc_vector
+                label_topic_vector[label] = [name]
+    return label_topic_vector
+
+def generateTopicLabelMatrix(model, metadata, data, files_dictionary):
+# For each article A
+#   For each MeSH label associated with A
+#       For each dimension j of the topic-document vector v[A]
+#           Topic-label-M[label][j] += v[A][j]
+    topic_label_matrix = {}
+    for text in data:
+        for mesh_label in metadata[text]:
+            topic_doc_vector = model[text]
+            for j in topic_doc_vector:
+                if mesh_label in topic_label_matrix.keys():
+                    topic_label_matrix[mesh_label].append(j)
+                else:
+                    topic_label_matrix[mesh_label] = [j]
+    return topic_label_matrix
 
 metadata = readMeta()
-topic_doc_vector = generateTopicDocVector(metadata)
+label_topic_vector = generateLabelDocVector(metadata)
+
+#format: [[SocialChange_5, [word, word, word]], etc.]
 files = readInDocuments('../_corpus/')
-files = preprocessData(files)
-dictionary = Dictionary(files)
-corpus = [dictionary.doc2bow(doc) for doc in files]
+
+#use half the data set for training
+training = files[:1000]
+trainingText = [text[1] for text in training]
+dictionary = Dictionary(trainingText)
+corpus = [dictionary.doc2bow(text) for text in trainingText]
 
 #this is only to load the dictionary
 temp = dictionary[0]
 id2word = dictionary.id2token
 
+#create model with training data
 model = LdaModel(corpus=corpus, id2word=id2word, num_topics=10)
-top_topics = model.top_topics(corpus)
-#print(top_topics[0])
 
-#For each article A
-#   For each MeSH label x associated with A
-#       For each dimension j of the topic-document vector v[A]
-#           Topic-label-M[x][j] += v[A][j]
-for text in files:
-    for mesh_label in metadata[text]:
-        for j in topic_doc_vector:
-            #do something here
+#use other half for testing
+testing = files[1000:]
+testingText = [text[1] for text in testing]
+test_dictionary = Dictionary(testingText)
+test_corpus = [test_dictionary.doc2bow(text) for text in testingText]
+
+vector = model[test_corpus[2]]
+print(vector)
+for result in vector:
+    print(test_dictionary[result[0]])
+
+print(generateTopicLabelMatrix(model, metadata, testing, filesDict))
+            
 
 
